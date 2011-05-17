36be6afe1f4996d4a370fcd8a78b3178adacbdd9
David Thompson <davexunit@gmail.com>
2011-05-17 18:31:22 -0400

    Added database module

---
+++ database.py
@@ -0,0 +1,11 @@
+class Game:
+	def __init__(self):
+		self.title = 'Untitled'
+		self.platform = 'NONE'
+
+class FAQ:
+	def __init__(self):
+		self.title = 'Untitled FAQ'
+		self.author = 'Anonymous'
+		self.date = '1/1/2001'
+		self.version = '1.0'
