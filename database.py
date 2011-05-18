ca4c48d848bb14f4af65fa79cddc23d962d21c07
David Thompson <davexunit@gmail.com>
2011-05-17 23:01:55 -0400

    Added a main FAQScraper class for program state control for elegance and to reset curses properly after using subprocess

--- database.py
+++ database.py
@@ -1,11 +1,29 @@
+import sqlite3
+
 class Game:
+	'''Encapsulates an entry in the game table.
+	'''
 	def __init__(self):
 		self.title = 'Untitled'
 		self.platform = 'NONE'
+	
+	def __init__(self, title, platform):
+		self.title = title
+		self.platform = platform
 
 class FAQ:
+	'''Encapsulates an entry in the faq table.
+	'''
 	def __init__(self):
 		self.title = 'Untitled FAQ'
 		self.author = 'Anonymous'
 		self.date = '1/1/2001'
 		self.version = '1.0'
+	
+	def __init__(self, title, author, date, version):
+		self.title = title
+		self.author = author
+		self.date = date
+		self.version = version
+
+
